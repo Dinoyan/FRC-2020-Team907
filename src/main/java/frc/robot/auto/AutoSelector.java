fba96e635800212fb9f5f750f8b49faf78ef16f0
Dinoyan Ganeshalingam <dinoyan.g98@gmail.com>
2020-01-06 18:59:13 -0500

    Mode chooser

--- src/main/java/frc/robot/auto/AutoSelector.java
+++ src/main/java/frc/robot/auto/AutoSelector.java
@@ -7,8 +7,48 @@
 
 package frc.robot.auto;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
 /**
  * Add your docs here.
  */
 public class AutoSelector {
+
+    private static final String kJustDrive = "Default";
+    private static final String kCenterShoot = "Center Shoot";
+
+    private final SendableChooser<String> mChooser = new SendableChooser<>();
+
+    private String mSelected;
+    private byte mMode;
+
+    private static AutoSelector mInstance = null;
+
+    public static AutoSelector getInstance() {
+        if (mInstance == null) {
+            mInstance = new AutoSelector();
+        }
+
+        return mInstance;
+    }
+
+    public AutoSelector() {
+        mChooser.setDefaultOption("Just Drive", kJustDrive);
+        mChooser.addOption("Center Shoot", kCenterShoot);
+    }
+
+    public byte getAutoMode() {
+        mSelected = mChooser.getSelected();
+
+        switch(mSelected) {
+            case kJustDrive:
+                mMode = 1;
+                break;
+            
+            case kCenterShoot:
+                mMode = 2;
+                break;
+        }
+        return mMode;
+    }
 }
