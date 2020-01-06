b0e8934f8f8c75e79aaef3318b86916640c883a7
Dinoyan Ganeshalingam <dinoyan.g98@gmail.com>
2020-01-06 11:01:03 -0500

    Create CrashTracker.java

---
+++ src/main/java/frc/robot/util/CrashTracker.java
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.util;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * Add your docs here.
+ */
+public class CrashTracker {
+    public static void logRobotConstruction() {
+        logNow("robot startup");
+    }
+
+    public static void logRobotInit() {
+        logNow("robot init");
+
+    }
+
+    public static void logTeleopInit() {
+        logNow("teleop init");
+
+    }
+
+    public static void logAutoInit() {
+        logNow("auto init");
+
+    }
+
+    public static void logDisabledInit() {
+        logNow("disable init");
+    }
+
+    public static void logTestInit() {
+        logNow("test init");
+
+    }
+
+    public static void SubsystemsDisgonitics() {
+        boolean mPass = true;
+
+        SmartDashboard.putBoolean("Subsystems", mPass);
+    }
+
+    private static void logNow(String mode) {
+        // Logger tracker = Logger.getLogger("Crash Tracker");
+        // FileHandler logFile;
+
+        // try {
+        //     logFile = new FileHandler("C:/");
+        //     tracker.addHandler(logFile);
+
+        //     tracker.info(mode);
+
+        // } catch (SecurityException e) {
+        //     e.printStackTrace();
+
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
+    }
+}
