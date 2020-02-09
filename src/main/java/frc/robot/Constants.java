c239ea58811c76c7c8d5b1b2f5e425a842ffc138
Dinoyan Ganeshalingam <dinoyan.g98@gmail.com>
2020-02-08 19:08:21 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,24 +14,29 @@ public class Constants {
     public final static int[] DRIVE_LEFT = {2, 3};
     
     // ********************* HOOK **************************
-    public final static int HOOK_CAN = 4;
+    public final static int HOOK_CAN = 8;
 
     // ******************** SHOOTER ************************
-    public final static int[] SHOOTER_CAN = {5, 6};
+    public final static int[] SHOOTER_CAN = {4, 5};
 
-    public final static int[] HOOD_PISTON = {4, 5};
+    public final static int[] HOOD_PISTON = {6, 7};
+
+    public final static double DEFAULT_CONVEYOR_SPEED = 0.0;
+    public final static double DEFAULT_SHOOTER_SPEED = 0.0;
+
+    public final static double CONTROL_CONVEYOR_SPEED = 1.0;
 
     // ******************* CW PANEL ************************
     public final static int CW_CAN = 7;
 
     // ******************** INTAKE *************************
-    public final static int[] INTAKE_MOTORS = {8, 9};
-    public final static int[] FRONT_PISTONS = {0, 1};
-    public final static int[] BACK_PISTONS = {2, 3};
+    public final static int[] INTAKE_MOTORS = {0, 1};
+    public final static int[] FRONT_PISTONS = {3, 2};
+    public final static int[] BACK_PISTONS = {0, 1};
 
-    public final static int CONVEYOR_MOTOR = 10;
+    public final static int CONVEYOR_MOTOR = 6;
 
-    public final static double INTAKE_ROLLER_SPEED = 1.0;
+    public final static double INTAKE_ROLLER_SPEED = 0.75;
     public final static double VOMIT_SPEED = -0.5;
 
     // ****************** JOYSTICKS ************************
@@ -43,9 +48,9 @@ public class Constants {
 
     public final static int CW_BUTTON = 2;
 
-    public final static int FRONT_INTAKE = 6;
-    public final static int BACK_INTAKE = 5;
-    public final static int EJECT = 6;
+    public final static int FRONT_INTAKE = 5;
+    public final static int BACK_INTAKE = 6;
+    public final static int EJECT = 10;
 
     public final static int SHOOT_AUTO = 4;
     public final static int SHOOT_MANUALLY_AXIS = 3;
@@ -61,6 +66,5 @@ public class Constants {
 
     // ********************* MISC ***************************
     public final static int AUTO_TIME = 15;
-
 
 }
